b4f4c2e34b27e396ceffde5cc6ec25b2ac01ce4c
Ramona Stoica <ramona.stoica1503@gmail.com>
2018-04-20 11:18:36 +0300

    display products + admin - add a Product

---
+++ src/main/java/com/emusicstore/model/Product.java
@@ -0,0 +1,109 @@
+package com.emusicstore.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    // @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "productId")
+    private int productId;
+
+    @Column(name = "productName")
+    private String productName;
+
+    @Column(name = "productCategory")
+    private String productCategory;
+
+    @Column(name = "productDescription")
+    private String productDescription;
+
+    @Column(name = "productPrice")
+    private double productPrice;
+
+    @Column(name = "productCondition")
+    private String productCondition;
+
+    @Column(name = "productStatus")
+    private String productStatus;
+
+    @Column(name = "unitInStock")
+    private int unitInStock;
+
+    @Column(name = "productManufacturer")
+    private String productManufacturer;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductCondition() {
+        return productCondition;
+    }
+
+    public void setProductCondition(String productCondition) {
+        this.productCondition = productCondition;
+    }
+
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public int getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(int unitInStock) {
+        this.unitInStock = unitInStock;
+    }
+
+    public String getProductManufacturer() {
+        return productManufacturer;
+    }
+
+    public void setProductManufacturer(String productManufacturer) {
+        this.productManufacturer = productManufacturer;
+    }
+}
